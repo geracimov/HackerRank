3e79cb720fb20f8ebc107d289fa4b9f9f1b53bc0
geracimov <geracimov@gmail.com>
2022-11-24 23:40:15 +0300

    java_negative_subarray test

--- src/main/java/ru/geracimov/hackerrank/java_negative_subarray/Solution.java
+++ src/main/java/ru/geracimov/hackerrank/java_negative_subarray/Solution.java
@@ -1,12 +1,27 @@
 package ru.geracimov.hackerrank.java_negative_subarray;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    public static void main(String[] args) throws IOException {
+        var bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+        int[] ints = Arrays.stream(bufferedReader.readLine().split("\\s"))
+                .map(Integer::parseInt)
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        System.out.println(solve(ints));
     }
 
     public static int solve(int[] ints) {
+        if (ints.length == 0) return 0;
+        if (ints.length == 1) return ints[0] < 0 ? 1 : 0;
+
         int count = 0;
         for (int start = 0; start < ints.length; start++) {
             int sum = ints[start];
--- src/test/java/ru/geracimov/hackerrank/java_negative_subarray/SolutionTest.java
+++ src/test/java/ru/geracimov/hackerrank/java_negative_subarray/SolutionTest.java
@@ -7,7 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SolutionTest {
 
     @Test
-    void solve() {
+    void solveCorrectTest() {
         assertThat(Solution.solve(new int[]{1, -2, 4, -5, 1})).isEqualTo(9);
+    }
+
+    @Test
+    void solveOnlyNegativeTest() {
+        assertThat(Solution.solve(new int[]{-1})).isEqualTo(1);
+    }
+
+    @Test
+    void solveOnlyZeroTest() {
+        assertThat(Solution.solve(new int[]{0})).isEqualTo(0);
+    }
+
+    @Test
+    void solveEmptyTest() {
+        assertThat(Solution.solve(new int[]{})).isEqualTo(0);
     }
 }
